b6b275cc617324b76d426daf7f2b9d3899dbd857
Anlor <chenxiex@outlook.com>
2024-05-10 17:16:30 +0800

    添加文件输入输出，批量处理
    添加从/data文件夹中读取文件，批量处理后输出到/data文件夹中。

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from nltk.parse.stanford import StanfordDependencyParser
 from nltk.parse.stanford import StanfordParser
@@ -12,51 +13,55 @@ from nltk.tree import Tree
 # os.environ['JAVA_HOME'] = java_path
 
 # 新闻标题和第一段正文，在后面只对正文做处理
-title = '苏伊上运河恢复通航!搁浅货轮已完全恢复至正常航道'
-text = "新华社快讯埃及苏伊士运河管理局29日发布公报说搁浅货轮已经完全恢复至正常航道。23日，一艘悬挂巴拿马国旗的重型货轮在苏伊士运河新航道搁浅，造成航道拥堵。25日，苏伊士运河管理局正式宣布运河暂停航行。就在不久前，埃及总统塞西曾宣布苏伊士运河河道的疏通工作成功完成。"
+with open('data/news.txt', 'r', encoding='utf-8') as f:
+    lines=f.readlines()
 
+for  i in range(len(lines)//2):
+    title=lines[2*i].strip()
+    text=lines[2*i+1].strip()
+    sys.stdout=open('data/output'+str(i)+'.txt','w')
+    def tree(x):
+        x = x.replace(',', '')
+        x = x.replace('Tree', '')
+        x = x.replace('[', '')
+        x = x.replace(']', '')
+        x = x.replace('\'', '')
+        return x
 
-def tree(x):
-    x = x.replace(',', '')
-    x = x.replace('Tree', '')
-    x = x.replace('[', '')
-    x = x.replace(']', '')
-    x = x.replace('\'', '')
-    return x
+    # 分词
+    segmenter = StanfordSegmenter(
+        java_class='edu.stanford.nlp.ie.crf.CRFClassifier',
+        path_to_jar=r"lab1/stanford-segmenter-4.2.0.jar",
+        path_to_slf4j=r"lab1/slf4j-api.jar",
+        path_to_sihan_corpora_dict=r"lab1/data",
+        path_to_model=r"lab1/data/pku.gz",
+        path_to_dict=r"lab1/data/dict-chris6.ser.gz"
+    )
+    result = segmenter.segment(text)
+    print(result)
 
-# 分词
-segmenter = StanfordSegmenter(
-    java_class='edu.stanford.nlp.ie.crf.CRFClassifier',
-    path_to_jar=r"lab1/stanford-segmenter-4.2.0.jar",
-    path_to_slf4j=r"lab1/slf4j-api.jar",
-    path_to_sihan_corpora_dict=r"lab1/data",
-    path_to_model=r"lab1/data/pku.gz",
-    path_to_dict=r"lab1/data/dict-chris6.ser.gz"
-)
-result = segmenter.segment(text)
-print(result)
+    # 命名实体识别
+    chi_tagger = StanfordNERTagger(
+        model_filename=r'lab1/chinese.misc.distsim.crf.ser.gz',
+        path_to_jar=r'lab1/stanford-ner-4.2.0.jar')
+    for word, tag in chi_tagger.tag(result.split()):
+        print(word, tag)
+    
+    # 句法分析
+    chi_parser = StanfordParser(r"lab1/stanford-parser.jar",
+                                r"lab1/stanford-parser-4.2.0-models.jar",
+                                r"lab1/chineseFactored.ser.gz")
 
-# 命名实体识别
-chi_tagger = StanfordNERTagger(
-    model_filename=r'lab1/chinese.misc.distsim.crf.ser.gz',
-    path_to_jar=r'lab1/stanford-ner-4.2.0.jar')
-for word, tag in chi_tagger.tag(result.split()):
-    print(word, tag)
+    x = str(list(chi_parser.parse(result.split())))
+    t = tree(x)
 
-# 句法分析
-chi_parser = StanfordParser(r"lab1/stanford-parser.jar",
-                            r"lab1/stanford-parser-4.2.0-models.jar",
-                            r"lab1/chineseFactored.ser.gz")
-
-x = str(list(chi_parser.parse(result.split())))
-t = tree(x)
-
-# 依存关系分析
-chi_parser = StanfordDependencyParser(r"lab1/stanford-parser.jar",
-                                      r"lab1/stanford-parser-4.2.0-models.jar",
-                                      r"lab1/chineseFactored.ser.gz")
-res = list(chi_parser.parse(result.split()))
-for row in res[0].triples():
-    print(row)
-
-Tree.fromstring(t).draw()  # 输出句法树
+    # 依存关系分析
+    
+    chi_parser = StanfordDependencyParser(r"lab1/stanford-parser.jar",
+                                        r"lab1/stanford-parser-4.2.0-models.jar",
+                                        r"lab1/chineseFactored.ser.gz")
+    res = list(chi_parser.parse(result.split()))
+    for row in res[0].triples():
+        print(row)
+    
+    Tree.fromstring(t).draw()  # 输出句法树
